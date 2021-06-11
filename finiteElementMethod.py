0205e93ae8775ed8dcf6298a0ddac256594e41e6
a18735215 <avshestakov@sberbank.ru>
2021-06-11 23:09:06 +0300

    refactoring

--- finiteElementMethod.py
+++ finiteElementMethod.py
@@ -8,7 +8,7 @@ import utils.equation as eq
 Метод конечных элементов с помощью функций-крышек
 Вид уравнения :  - d/dx(  p(x)dy/dx) + C * q(x)y = A * f(x)
 '''
-def solve(A, C, N, h, x):
+def solve(A, C, n, h, x):
     # Инициализируем векторы для вычисления интегралов
     Q1 = []
     Q2 = []
@@ -17,7 +17,7 @@ def solve(A, C, N, h, x):
     Q5 = []
     Q6 = []
     # аппроксимация 6 интегралов
-    for i in range(N-1):
+    for i in range(n-1):
         q1 = (h[i] / 12) * (eq.q(x[i],C) + eq.q(x[i+1],C))
         Q1.append(q1)
         q2 = (h[i-1] / 12) * (3 * eq.q(x[i],C) + eq.q(x[i-1],C))
@@ -31,35 +31,35 @@ def solve(A, C, N, h, x):
         q6 = (h[i] / 6) * (2 * eq.f(x[i],A) + eq.f(x[i+1],A))
         Q6.append(q6)
 
-    q1n = (h[N]/12)*(eq.q(x[N-1],C) + eq.q(x[N],C))
+    q1n = (h[n]/12)*(eq.q(x[n-1],C) + eq.q(x[n],C))
     Q1.append(q1n)
-    q2n = (h[N-2] / 12) * (3 * eq.q(x[N-1],C) + eq.q(x[N-2],C))
+    q2n = (h[n-2] / 12) * (3 * eq.q(x[n-1],C) + eq.q(x[n-2],C))
     Q2.append(q2n)
-    q3n = (h[N] / 12) * (3 * eq.q(x[N-1],C) + eq.q(x[N],C))
+    q3n = (h[n] / 12) * (3 * eq.q(x[n-1],C) + eq.q(x[n],C))
     Q3.append(q3n)
-    q4n_last = (1 / (2 * h[N-1]) ) * (eq.p(x[N]) + eq.p(x[N-1]))
-    q4n_second_last = (1 / (2 * h[N-2]) ) * (eq.p(x[N-1]) + eq.p(x[N-2]))
+    q4n_last = (1 / (2 * h[n-1])) * (eq.p(x[n]) + eq.p(x[n-1]))
+    q4n_second_last = (1 / (2 * h[n-2])) * (eq.p(x[n-1]) + eq.p(x[n-2]))
     Q4.append(q4n_second_last)
     Q4.append(q4n_last)
-    q5n = (h[N-1] / 6) * (2 * eq.f(x[N-1],A) + eq.f(x[N-2],A))
+    q5n = (h[n-1] / 6) * (2 * eq.f(x[n-1],A) + eq.f(x[n-2],A))
     Q5.append(q5n)
-    q6n = (h[N] / 6) * (2 * eq.f(x[N-1],A) + eq.f(x[N],A))
+    q6n = (h[n] / 6) * (2 * eq.f(x[n-1],A) + eq.f(x[n],A))
     Q6.append(q6n)
 
-    alpha = np.zeros(N+1)
-    beta = np.zeros(N+1)
-    b = np.zeros(N+1)
-    a = np.zeros(N+1)
-    for i in range(N-1):
+    alpha = np.zeros(n+1)
+    beta = np.zeros(n+1)
+    b = np.zeros(n+1)
+    a = np.zeros(n+1)
+    for i in range(n-1):
         alpha[i] = Q4[i] + Q4[i+1] + Q2[i] + Q3[i]
         beta[i] = - Q4[i+1] + Q1[i]
         b[i] = Q5[i] + Q6[i]
-    alpha[N-1] = Q4[N-1] + Q4[N] + Q2[N-1] + Q3[N-1]
-    b[N-1] = Q5[N-1] + Q6[N-1]
+    alpha[n-1] = Q4[n-1] + Q4[n] + Q2[n-1] + Q3[n-1]
+    b[n-1] = Q5[n-1] + Q6[n-1]
     a[0] = alpha[0]
 
-    c = tma.solve(N, a, b, alpha, beta)
-    u = func.basis(N, h, x)
+    c = tma.solve(n, a, b, alpha, beta)
+    u = func.basis(n, h, x)
 
     y = np.dot(u, c)
 
--- tridiagonalMatrixAlgorithm.py
+++ tridiagonalMatrixAlgorithm.py
@@ -4,27 +4,27 @@ import utils.logger as logger
 '''
 Расчет СЛАУ
 '''
-def solve(N, a, b, alpha, beta):
+def solve(n, a, b, alpha, beta):
     logger.log(text="Начинаем расчет СЛАУ", force=True)
 
-    zeta = np.zeros(N + 1)
-    z = np.zeros(N + 1)
-    c = np.zeros(N + 1)
+    zeta = np.zeros(n + 1)
+    z = np.zeros(n + 1)
+    c = np.zeros(n + 1)
     zeta[0] = beta[0] / alpha[0]
     z[0] = b[0] / a[0]
-    for i in range(1, N - 1):
+    for i in range(1, n - 1):
         a[i] = alpha[i] - beta[i - 1] * zeta[i - 1]
         zeta[i] = beta[i] / a[i]
         z[i] = (b[i] - beta[i - 1] * z[i - 1]) / a[i]
-    a[N - 1] = alpha[N - 1] - beta[N - 2] * zeta[N - 2]
-    z[N - 1] = (b[N - 1] - beta[N - 2] * z[N - 2]) / a[N - 1]
+    a[n - 1] = alpha[n - 1] - beta[n - 2] * zeta[n - 2]
+    z[n - 1] = (b[n - 1] - beta[n - 2] * z[n - 2]) / a[n - 1]
 
     # переворачиваем список
     indices = []
-    for i in range(N):
-        a = N - 1 - i
+    for i in range(n):
+        a = n - 1 - i
         indices.append(a)
-    c[N] = z[N]
+    c[n] = z[n]
 
     for i in indices:
         c[i] = z[i] - zeta[i] * c[i + 1]
